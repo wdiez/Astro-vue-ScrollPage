983b256bb4f4645df14f4ad14c4938d8aaaf1089
Walter Diez <wdiez3@gmail.com>
2024-04-16 12:34:09 -0300

    integrando vue

---
+++ src/components/ScrollPage.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h1>{{ hello }}</h1>
+        <input type="text" v-model="hello">
+    </div>
+</template>
+
+<script>
+    export default {
+        data(){
+            return {
+                hello: "Hola vue" 
+            }
+        }
+    }
+</script>
