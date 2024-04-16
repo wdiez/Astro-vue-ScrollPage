ab0c75c0731445bf82dc59be405aadd066bbab78
Walter Diez <wdiez3@gmail.com>
2024-04-16 12:58:13 -0300

    generando scroll a nuetra pagina

--- src/components/ScrollPage.vue
+++ src/components/ScrollPage.vue
@@ -1,7 +1,23 @@
 <template>
-    <div>
-        <h1>{{ hello }}</h1>
-        <input type="text" v-model="hello">
+    <div class="slider" ref="slider">
+        <div class="navbar">
+            <a href="#" @click="moveScroll(1)">HOME</a>
+            <a href="#" @click="moveScroll(2)">DOS</a>
+            <a href="#" @click="moveScroll(3)">TRES</a>
+            <a href="#" @click="moveScroll(4)">CUATRO</a>
+        </div>
+        <div class="content_page">
+            <h1>Home</h1>
+        </div>
+        <div class="content_page">
+            <h1>Contenido 2</h1>
+        </div>
+        <div class="content_page">
+            <h1>Contenido 3</h1>
+        </div>
+        <div class="content_page">
+            <h1>Contenido 4</h1>
+        </div>
     </div>
 </template>
 
@@ -9,8 +25,32 @@
     export default {
         data(){
             return {
-                hello: "Hola vue" 
+                slider: null,
+                height: null, 
             }
-        }
+        },
+        mounted(){
+            this.slider = this.$refs.slider;
+            this.height = this.slider.offsetHeight;
+        },
+        methods:{
+            moveScroll(n){
+                n = n-1,
+                this.slider.scrollTo({
+                    top:this.height * n,
+                    behavior: 'smooth'
+                })
+            }
+            
+            
+        } 
     }
 </script>
+
+<style>
+    .slider{
+        position: relative;
+        height: 100vh;
+        overflow: scroll;
+    }
+</style>
